8ef501eb842b6cebdbbf0c41d42a210ace834454
andrewhought <80929614+boolin9@users.noreply.github.com>
2022-07-23 09:43:18 -0700

    game setup

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 from turtle import Turtle, Screen
+import random
+
+
+def left():
+    snake.setheading(180)
+
+def right():
+    snake.setheading(0)
+
+def up():
+    snake.setheading(90)
+
+def down():
+    snake.setheading(270)
+
+
+screen = Screen()
+snake = Turtle('turtle')
+screen.bgcolor('black')
+screen.title("Snake Game")
+screen.listen()
+screen.onkey(left, "Left")
+screen.onkey(right, "Right")
+screen.onkey(up, "Up")
+screen.onkey(down, "Down")
+
+while True:
+    snake.pu()
+    snake.forward(1)
+
+
+
+
+screen.listen()
+
+screen.exitonclick()
